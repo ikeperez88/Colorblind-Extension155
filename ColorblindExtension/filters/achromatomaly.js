362da75c1a6898ca381cf74214cd54be988f04f2
ikeperez88 <99516732+ikeperez88@users.noreply.github.com>
2023-04-28 20:22:24 -0700

    Update achromatomaly.js

--- ColorblindExtension/filters/achromatomaly.js
+++ ColorblindExtension/filters/achromatomaly.js
@@ -1,18 +1,23 @@
 // achromatomaly
 
+//clears currently applied filters to apply another
 if (document.getElementById("styleID612481")) {
     stylingID = document.getElementById("styleID612481").remove();
     filterID = document.getElementById("filterID471924").remove();
 }
+
+//sets styling ID
 stylingID = document.createElement('style');
 stylingID.id = "styleID612481";
 document.body.appendChild(stylingID);
 
+//sets fitler ID
 filterID = document.createElement('div');
 filterID.id = "filterID471924";
 filterID.setAttribute('style', 'height: 0; padding: 0; margin: 0; line-height: 0;');
 document.body.appendChild(filterID);
 
+//creates a color matrix to send to colorblind listener for filtering
 filterID.innerHTML = '<svg id="colorblind-filters" style="display: none"> <defs> <filter id="achromatomaly"> <feColorMatrix type="matrix" values="0.618,0.320,0.062,0,0 0.163,0.775,0.062,0,0 0.163,0.320,0.516,0,0 0,0,0,1,0" in="SourceGraphic" /> </filter> </defs> </svg>';
 stylingID.innerHTML = 'html{-webkit-filter:url(#achromatomaly);-moz-filter:(#achromatomaly);-ms-filter:(#achromatomaly);-o-filter:(#achromatomaly);filter:(#achromatomaly);}'
 setTimeout(function() {
